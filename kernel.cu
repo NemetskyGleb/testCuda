5465ccb21e22e9a717b215db1fd80a1153409a37
Nemetsky Gleb <glebdvp@gmail.com>
2022-11-13 00:34:08 +0700

    Add project files.

---
+++ kernel.cu
@@ -0,0 +1,61 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <iostream>
+
+cudaError_t addWithCuda();
+
+__global__ void addKernel()
+{
+    printf("%d, %d", threadIdx.x, threadIdx.y);
+}
+
+int main()
+{
+    cudaError_t cudaStatus = addWithCuda();
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
+
+// Helper function for using CUDA to add vectors in parallel.
+cudaError_t addWithCuda()
+{
+    cudaError_t cudaStatus;
+
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        std::exit(-1);
+    }
+
+    // Launch a kernel on the GPU with one thread for each element.
+    addKernel<<<1, 12>>>();
+
+    // Check for any errors launching the kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        std::exit(-1);
+    }
+    
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        std::exit(-1);
+    }
+
+    return cudaStatus;
+}
